ff998cc943edae58193a6acb34fd5749da968af9
Filippov-Nikolay <filippovnikolay195@gmail.com>
2024-01-17 12:59:15 +0200

    Исправил отображение ошибок на странице video.html

--- js/main.js
+++ js/main.js
@@ -74,6 +74,86 @@ $(document).ready(function() {
         // $(".right-bar-avatar").toggleClass("right-bar-avatar--active");
     });
 
+
+    // TEMPLATE CONTENT
+    let url = "../database/listVideo.json";
+    let template = document.querySelector("#template");
+
+    // PAGE CATEGORIES
+    let top_10_for_the_week = document.querySelector("#top_10_for_the_week .video-about");
+    let continue_watching = document.querySelector("#continue_watching .video-about");
+    let popular = document.querySelector("#popular .video-about");
+    let all = document.querySelector("#all .video-about");
+
+    function formatNumber(viewsCount) {
+        if (viewsCount <= 9000) {
+            return `${viewsCount.substring(0, 1)}K views`;
+        } else if (viewsCount <= 99000) {
+            return `${viewsCount.substring(0, 2)}K views`;
+        } else if (viewsCount <= 999000) {
+            return `${viewsCount.substring(0, 3)}K views`;
+        } else if (viewsCount <= 9999000) {
+            return `${viewsCount.substring(0, 1)}M views`;
+        } else if (viewsCount <= 99999000) {
+            return `${viewsCount.substring(0, 2)}M views`;
+        } else if (viewsCount <= 999999000) {
+            return `${viewsCount.substring(0, 3)}M views`;
+        } else if (viewsCount <= 9999999000) {
+            return `${viewsCount.substring(0, 1)}B views`;
+        } else if (viewsCount <= 99999999000) {
+            return `${viewsCount.substring(0, 2)}B views`;
+        } else {
+            return `${viewsCount} views`;
+        }
+    }
+
+    if (template != null) {
+        let templateClone = template.content.cloneNode(true);
+        let content = $(".video-about");
+    
+        console.log(templateClone);
+        console.log(content);
+    
+        fetch(url)
+        .then(response => response.json()) // десериализует объект из ответа в JSON формате
+        .then(json => {
+            console.log(Object.keys(json[0]));
+            
+            let size = json.length;
+    
+            for (let i = 0; i < size; i++) {
+                templateClone = template.content.cloneNode(true);
+    
+                templateClone.querySelector(".video-img img").setAttribute("src", json[i].path_to_video_photo);
+                templateClone.querySelector(".channel__link-img").setAttribute("src", json[i].channel_image_title);
+                templateClone.querySelector(".link__video").setAttribute("href", `video.html${json[i].video_url}` );
+                templateClone.querySelector(".name-video__link").innerHTML = json[i].video_title;
+                templateClone.querySelector(".channel-name__link").innerHTML = json[i].channel_title;
+                templateClone.querySelector(".video-content__number-views").innerHTML = formatNumber(String(json[i].views_count));
+                templateClone.querySelector(".video-content__metadata").innerHTML = json[i].video_age;
+    
+                for (let j = 0; j < json[i].category.length; j++) {
+                    if (json[i].category[j] == "top_10_for_the_week") {
+                        top_10_for_the_week.append(templateClone);
+                        console.log(1);
+                    } else if (json[i].category[j] == "continue_watching") {
+                        continue_watching.append(templateClone);
+                        console.log(2);
+                    } else if (json[i].category[j] == "popular") {
+                        popular.append(templateClone);
+                        console.log(3);
+                    } else if (json[i].category[j] == "all") {
+                        all.append(templateClone);
+                        console.log(4);
+                    }
+                }
+                // content[0].append(templateClone);
+            }
+        });
+    }
+
+
+
     // CATEGORY
     let isLongClick = true;
 
@@ -88,29 +168,31 @@ $(document).ready(function() {
     let startX;
     let scrollLeft;
     
-    const slider = document.querySelector('.category__slider'); // замените '.items' на селектор вашего элемента
+    const slider = document.querySelector('.category__slider');
     
-    slider.addEventListener('mousedown', (e) => {
-        isDown = true;
-        startX = e.pageX - slider.offsetLeft;
-        scrollLeft = slider.scrollLeft;
-    });
-
-    slider.addEventListener('mousemove', (e) => {
-        if(!isDown) return;
-        e.preventDefault();
-        const x = e.pageX - slider.offsetLeft;
-        const walk = x - startX;
-        slider.scrollLeft = scrollLeft - walk;
-    });
-
-    slider.addEventListener('mouseup', () => {
-        isDown = false;
-    });
-
-    slider.addEventListener('mouseleave', () => {
-        isDown = false;
-    });
+    if (slider) {
+        slider.addEventListener('mousedown', (e) => {
+            isDown = true;
+            startX = e.pageX - slider.offsetLeft;
+            scrollLeft = slider.scrollLeft;
+        });
+    
+        slider.addEventListener('mousemove', (e) => {
+            if(!isDown) return;
+            e.preventDefault();
+            const x = e.pageX - slider.offsetLeft;
+            const walk = x - startX;
+            slider.scrollLeft = scrollLeft - walk;
+        });
+    
+        slider.addEventListener('mouseup', () => {
+            isDown = false;
+        });
+    
+        slider.addEventListener('mouseleave', () => {
+            isDown = false;
+        });
+    }
 
     
     $(".category__slider").mousedown(function() {
@@ -185,81 +267,6 @@ $(document).ready(function() {
             });
         }
         isLongClick = true;
-    });
-
-
-    // TEMPLATE CONTENT
-    let url = "../database/listVideo.json";
-    let template = document.querySelector("#template");
-    let templateClone = template.content.cloneNode(true);
-    let content = $(".video-about");
-
-    console.log(templateClone);
-    console.log(content);
-
-    function formatNumber(viewsCount) {
-        if (viewsCount <= 9000) {
-            return `${viewsCount.substring(0, 1)}K views`;
-        } else if (viewsCount <= 99000) {
-            return `${viewsCount.substring(0, 2)}K views`;
-        } else if (viewsCount <= 999000) {
-            return `${viewsCount.substring(0, 3)}K views`;
-        } else if (viewsCount <= 9999000) {
-            return `${viewsCount.substring(0, 1)}M views`;
-        } else if (viewsCount <= 99999000) {
-            return `${viewsCount.substring(0, 2)}M views`;
-        } else if (viewsCount <= 999999000) {
-            return `${viewsCount.substring(0, 3)}M views`;
-        } else if (viewsCount <= 9999999000) {
-            return `${viewsCount.substring(0, 1)}B views`;
-        } else if (viewsCount <= 99999999000) {
-            return `${viewsCount.substring(0, 2)}B views`;
-        } else {
-            return `${viewsCount} views`;
-        }
-    }
-
-    // PAGE CATEGORIES
-    let top_10_for_the_week = document.querySelector("#top_10_for_the_week .video-about");
-    let continue_watching = document.querySelector("#continue_watching .video-about");
-    let popular = document.querySelector("#popular .video-about");
-    let all = document.querySelector("#all .video-about");
-
-    fetch(url)
-    .then(response => response.json()) // десериализует объект из ответа в JSON формате
-    .then(json => {
-        console.log(Object.keys(json[0]));
-        
-        let size = json.length;
-
-        for (let i = 0; i < size; i++) {
-            templateClone = template.content.cloneNode(true);
-
-            templateClone.querySelector(".video-img img").setAttribute("src", json[i].path_to_video_photo);
-            templateClone.querySelector(".channel__link-img").setAttribute("src", json[i].channel_image_title);
-            templateClone.querySelector(".link__video").setAttribute("href", `video.html${json[i].video_url}` );
-            templateClone.querySelector(".name-video__link").innerHTML = json[i].video_title;
-            templateClone.querySelector(".channel-name__link").innerHTML = json[i].channel_title;
-            templateClone.querySelector(".video-content__number-views").innerHTML = formatNumber(String(json[i].views_count));
-            templateClone.querySelector(".video-content__metadata").innerHTML = json[i].video_age;
-
-            for (let j = 0; j < json[i].category.length; j++) {
-                if (json[i].category[j] == "top_10_for_the_week") {
-                    top_10_for_the_week.append(templateClone);
-                    console.log(1);
-                } else if (json[i].category[j] == "continue_watching") {
-                    continue_watching.append(templateClone);
-                    console.log(2);
-                } else if (json[i].category[j] == "popular") {
-                    popular.append(templateClone);
-                    console.log(3);
-                } else if (json[i].category[j] == "all") {
-                    all.append(templateClone);
-                    console.log(4);
-                }
-            }
-            // content[0].append(templateClone);
-        }
     });
 
 
